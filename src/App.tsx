215371465c93ecd6b4404325104320937358727e
Shubham Shakya <shubhamshakya369@gmail.com>
2023-04-25 18:46:09 +0545

    migrate comment fetching to backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,33 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SentimentResults } from "./types/sentimentresult";
 import { getSentimentResults } from "./services/getsentiment.service";
 import { getComments } from "./services/getcomments.service";
 import BarChart from "./components/BarChart";
+import { getYoutubeSentiment } from "./services/youtubeanalysis.service";
+
+function isValidYoutubeVideo(url: string) {
+  const youtubeUrlPattern =
+    /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=|embed\/)?([a-zA-Z0-9_-]{11})/;
+  return youtubeUrlPattern.test(url);
+}
 
 function App() {
-  const [comments, setComments] = useState<string[]>([]);
-  const [valid, setValid] = useState<boolean>(true);
+  const [valid, setValid] = useState<boolean>(false);
   const [sentimentResults, setSentimentResults] = useState<SentimentResults>(
     {}
   );
+  const [currentTab, setCurrentTab] = useState<string | undefined>("");
+
+  useEffect(() => {
+    chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
+      const currentUrl = tabs[0].url;
+      setCurrentTab(currentUrl);
+      if (currentUrl) {
+        isValidYoutubeVideo(currentUrl) ? setValid(true) : setValid(false);
+      }
+    });
+  }, []);
 
   const handleClick = async () => {
     chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
@@ -18,10 +35,8 @@ function App() {
       if (currentUrl) {
         const videoId = getVideoId(currentUrl);
         if (videoId) {
-          const comments = await getComments(videoId);
-          setComments(comments);
-          const results = await getSentimentResults(comments);
-          setSentimentResults(results);
+          const data = await getYoutubeSentiment({ videoId: videoId });
+          setSentimentResults(data);
         } else {
           setValid(false);
         }
@@ -30,31 +45,12 @@ function App() {
   };
 
   return (
-    <div className="App flex flex-col items-center justify-center gap-3">
-      <h1 className="text-3xl font-bold underline">Senti-Surfer</h1>
-      <button onClick={handleClick}>Analyze</button>
-      <div className="mt-4 flex flex-col gap-2">
-        <p className="font-bold">
-          {Object.keys(sentimentResults).length != 0 ? (
-            <>
-              <p>
-                Average Sentiment Score : {calculateAverage(sentimentResults)}
-              </p>
-              <div className="w-96 h-96 bg-slate-400 flex items-center justify-center">
-                <BarChart test={sentimentResults} />
-              </div>
-            </>
-          ) : null}
-        </p>
-        {Object.keys(sentimentResults).map((result) => (
-          <div key={result} className="p-4 bg-gray-900 rounded-lg">
-            <h2 className="text-lg font-bold">{result}</h2>
-            <p className="mt-2">Score: {sentimentResults[result].compound}</p>
-          </div>
-        ))}
-      </div>
-
-      {!valid && <p className="text-red-700">Not a valid youtube video</p>}
+    <div className="App">
+      {valid ? (
+        <p className="text-red-500">Valid Shit</p>
+      ) : (
+        <p className="text-green-500">Invalid Shit</p>
+      )}
     </div>
   );
 }
